6104737446bc16c350a668bc878c88eacbd6ad6c
SaraCosta <sarafilipacosta@gmail.com>
2020-01-28 22:34:42 +0000

    Use yearfracdiff

--- pension/model/member.py
+++ pension/model/member.py
@@ -1,5 +1,6 @@
 from datetime import date
 from dateutil.relativedelta import relativedelta
+from pension.helper_functions import year_fractional_diff
 
 class Member:
     def __init__(
@@ -32,19 +33,19 @@ class Member:
         return date(self.birthdate.year+nra, self.birthdate.month, self.birthdate.day)
     
     def age_at_valuation_date(self, valuation_date):
-        return relativedelta(valuation_date,self.birthdate)
+        return year_fractional_diff(self.birthdate,valuation_date)
     
     def pensionable_service(self, valuation_date):
-        return relativedelta(valuation_date,self.pensionable_service_date)
+        return year_fractional_diff(self.pensionable_service_date,valuation_date)
     
     def member_service(self, valuation_date):
-        return relativedelta(valuation_date,self.membership_date)
+        return year_fractional_diff(self.membership_date,valuation_date)
     
     def prospective_service(self, nra, valuation_date):
-        return relativedelta(self.normal_retirement_date(nra),valuation_date)
+        return year_fractional_diff(valuation_date,self.normal_retirement_date(nra))
     
     def total_service(self, nra):
-        return relativedelta(self.normal_retirement_date(nra),self.pensionable_service_date)
+        return year_fractional_diff(self.pensionable_service_date,self.normal_retirement_date(nra))
 
 # Following a recent review, the government has announced plans to bring this timetable forward. 
 # The State Pension age would therefore increase to 68 between 2037 and 2039
@@ -52,7 +53,7 @@ class Member:
 # Date of birthday between 6 April 1970 and 5 april 1978 State pension age is currently 67. it would increase to between 67 yeras and 1 month, and 68, depending on date of birth
 # Date of birthday after 6 April 1978 The state Pension age remains 68                  
 
-    def state_pension_age (self):
+    def state_pension_age(self):
         if self.birthdate<=date(1948, 4, 5) and self.gender=="F":
             state_pension_age=60
         elif self.birthdate<=date(1948, 4, 5) and self.gender=="M":
@@ -62,14 +63,3 @@ class Member:
         elif self.birthdate>date(1978, 4, 6):
             state_pension_age=68
         return state_pension_age
-    
-    def pre97_service(self):
-        if self.membership_date>date(1997,4,6):
-            pre97_service=relativedelta()
-        else:
-            pre97_service=relativedelta(date(1997,4,6),self.membership_date)
-        return pre97_service
-    
-    
-            
-    
